154fb88cd7164c62fa404f713663e29c4ff77dd7
DominoWWW <Cilas@DominoWWW.com>
2024-11-22 15:54:26 +0100

    count number of unique counties

--- src/data_formaters/north_carolina/north_carolina.c
+++ src/data_formaters/north_carolina/north_carolina.c
@@ -1,37 +1,89 @@
+#include <stdbool.h>
+
 #include "header.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define COUNTY_NAME_LENGTH 30
+
+void copy_array(char from[], char to[]);
+int compare_strings(const char *a, const char *b, int length);
 
 int main(void) {
-    struct votes_t {
-        int election_day;
-        int early_voting;
-        int absentee_by_mail;
-        int provisional;
-        int total;
+    char party[][3] = {
+        "CST",
+        "DEM",
+        "GRE",
+        "LIB",
+        "REP",
+        "UNA",
     };
-    typedef struct votes_t votes_t;
 
-    struct raw_line_t {
-        char county[30];
-        char contest_name[60];
-        char party[3];
-        votes_t votes_t;
+    const int party_length = sizeof(party) / sizeof(party[0]);
+
+    struct county_t {
+        char name[COUNTY_NAME_LENGTH];
+        int district;
+        int votes[party_length];
     };
-    typedef struct raw_line_t raw_line_t;
+    typedef struct county_t county_t;
 
-    FILE *raw_file = fopen("north_carolina_raw_results_2024.txt", "r");
+    //Open file
+    FILE *raw_file = fopen("us_house_north_carolina_results_2024.csv", "r");
     if (raw_file == NULL) {
         printf("Error opening file\n");
         exit(EXIT_FAILURE);
     }
 
-    raw_line_t first_line;
     //skip first line
     fscanf(raw_file, "%*[^\n]\n");
-    fscanf(raw_file, "%s \t %*s \t %*s \t %*s \t %*s \t %[^\t]s", &first_line.county, &first_line.contest_name);
+
+    //count the number of unique counties
+    int number_of_counties = 0;
+    //check if county name n - 1 is different for county name n
+    char county_name[COUNTY_NAME_LENGTH];
+    char previous_county_name[COUNTY_NAME_LENGTH] = "AAAAAAAAAAAA";
+
+    //loop through file
+    while (feof(raw_file) == false) {
+        fscanf(raw_file, "%[^\t] \t %*i \t %*s \t %*i\n", &county_name);
+        // if (county_name == previous_county_name) number_of_counties++
+        if (compare_strings(county_name, previous_county_name, COUNTY_NAME_LENGTH) == 0) {
+            //printf("%s != %s\n", county_name, previous_county_name);
+            number_of_counties++;
+            memcpy(previous_county_name, county_name, COUNTY_NAME_LENGTH);
+        }
+    }
 
 
-    printf("%s \t %s\t %s", first_line.county, first_line.contest_name);
+    printf("The number of counties is: %d\n", number_of_counties);
+
+    //printf("%s \t %i \t %s \t %i\n", first_line.county, first_line.district, first_line.party, first_line.votes);
+
     return 0;
 }
+
+void copy_array(char *from, char *to) {
+    int from_length = sizeof(from);
+    int to_length = sizeof(to);
+
+    if (from_length != to_length) {
+        printf("Error in copy_array(): array length is different!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < from_length; i++) {
+        to[i] = from[i];
+    }
+}
+
+int compare_strings(const char *a, const char *b, int length) {
+    for (int i = 0; i < COUNTY_NAME_LENGTH; i++) {
+        if (a[i] != b[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
