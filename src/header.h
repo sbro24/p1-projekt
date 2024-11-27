8da081294a6d1ab0e98aa6446022b67206d50ea8
Skrimma <hhjart24@student.aau.dk>
2024-11-27 09:39:07 +0100

    fe

--- src/header.h
+++ src/header.h
@@ -2,6 +2,3 @@
 
 void test(void);
 
-void calculate_proportionality(struct district_t, int total_seats, int party_seats);
-
-void print_proportionality(struct district_t district);
--- src/project_test/calculate_proportionality.c
+++ src/project_test/calculate_proportionality.c
@@ -7,7 +7,6 @@
 #define DEM_TOTAL_VOTES 2328248;
 #define REP_TOTAL_VOTES 2889657;
 #define OTHER_TOTAL_VOTES 0;
-#define TOTAL_SEATS 14
 
 typedef struct  {
     int district_number;
@@ -23,46 +22,53 @@ typedef struct {
     double other_vote_percentage;
 } vote_percentage_t;
 
-int calculate_proportionality(district_t district, int party_seats) {
+
+vote_percentage_t calculate_district(district_t district, int total_seats);
+
+void print_district(district_t district, vote_percentage_t vote_result);
+
+vote_percentage_t calculate_percentages(district_t district, int total_seats) {
     // Input the values from district
-    char *name = district.district_number;
-    double total_votes = district.total_votes;
     double dem_votes = district.dem_votes;
     double rep_votes = district.rep_votes;
     double other_votes = district.other_votes;
 
     // Calculate the voting percentages
-    vote_percentage_t vote_percentage;
-    for (int i = 0; i <= TOTAL_SEATS; i++) {
+    for (int i = 0; i < total_seats; i++) {
+        vote_percentage_t vote_percentage;
         vote_percentage.dem_vote_percentage = 100 * dem_votes / TOTAL_VOTES;
         vote_percentage.rep_vote_percentage = 100 * rep_votes / TOTAL_VOTES;
         vote_percentage.other_vote_percentage = 100 * other_votes / TOTAL_VOTES;
     }
 
-
 }
 
-void print_proportionality(district_t district) {
+void print_district(district_t district, vote_percentage_t vote_results) {
     // Input the values from district
 
-    printf("%d:\n", district.district_number);
+    printf("District %d:\n", district.district_number);
     printf("District total votes: %d\n", district.total_votes);
     printf("District votes for democrats: %d\n", district.dem_votes);
     printf("District votes for republicans: %d\n",district.rep_votes);
     printf("District votes for other parties: %d\n'", district.other_votes);
     printf("\n");
 
-    printf("");
-
+    printf("Democratic total vote percentage: %d:\n", );
 }
+
+
 int main(void) {
-    district_t districts[2] = {
+    district_t districts[] = {
         {1, 354523, 214596, 139927, 0},
         {2, 314054,140651, 173403, 0}
     };
+    int number_of_districts = sizeof(districts) / sizeof(districts[0]);
+    int total_seats = number_of_districts;
 
-    for (int i = 0; i < strlen(districts); i++) {
-        print_proportionality(districts[i]);
+    for (int i = 0; i < number_of_districts; i++) {
+        vote_percentage_t vote_results = calculate_district(districts[i], number_of_districts);
+        print_district(districts[i], vote_results);
+
     }
 
 
