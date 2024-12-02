e8c56bbaae41f393958774f9808286ac27d72f4b
DominoWWW <Cilas@DominoWWW.com>
2024-12-02 15:16:38 +0100

    no settings :D

--- src/main.c
+++ src/main.c
@@ -2,7 +2,26 @@
 #include <stdio.h>
 
 int main(void) {
+    //configs - future virsion could load this from a file
+    char file_vote[] = "path";
+    char file_map[] = "path";
+    char file_graph[] = "path";
+    char file_population[] = "path";
+    char parties[][4] = {
+        "CST",
+        "DEM",
+        "GRE",
+        "LIB",
+        "REP",
+        "UNA",
+    };
+
+
+
     printf("Fucking cLion AAAAAAAAAAAAAAAAAAAAAAAAAA!\n");
-    settings();
+
+
+
+
     return 0;
 }
--- src/settings.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-
-void settings(void) {
-    printf("Fucking settings AAAAAAAAAAAaAAAAAAAAAAAAAAA!\n");
-}
