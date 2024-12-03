5f2038548315ca9b570440deebfda2bd204324d3
MikkelVintherJensen <mvje08@student.aau.dk>
2024-12-03 14:06:47 +0100

    first draft eval_map_2

--- src/eval_map/eval_map.c
+++ src/eval_map/eval_map.c
@@ -7,59 +7,56 @@ typedef struct {
     int y; // Column index
 } coordinate;
 
-double calc_radius (int count);
+//prototypes
 void read_district (int district[10][10]);
 void print_district (int district[10][10]);
 coordinate* generate_coordinates(int rows, int cols, int district[rows][cols], int* count);
-void calc_center (double* center_x, double* center_y, coordinate* coordinates, int count);
-double calc_dist (coordinate* coordinate_point, double center_x, double center_y);
 
-int main(void) {
+double eval_fill(int count, coordinate coordinates[count]);
+    double calc_radius (int count);
+    void check_coordinate(int count, coordinate coordinates[count], double radius);
+
+double eval_shape(int count, coordinate coordinates[count]);
+    double calc_dist (coordinate* coordinate_point, double center_x, double center_y);
+    double calc_avg_dist(int count, coordinate coordinates[count], double center_x, double center_y);
+    void calc_center (double* center_x, double* center_y, coordinate* coordinates, int count);
+
+int
+main(void) {
 
     int district[10][10];
+    int count = 0; // To store the number of coordinates generated
 
     read_district(district);
+    coordinate* coordinates = generate_coordinates(10, 10, district, &count);
     print_district(district);
 
-    int count = 0; // To store the number of coordinates generated
-    coordinate* coordinates = generate_coordinates(10, 10, district, &count);
-
-    double  center_x = 0,
-            center_y = 0;
-
-    calc_center(&center_x, &center_y, coordinates, count);
-
-    printf("Center x: %lf\n", center_x);
-    printf("Center y: %lf\n", center_y);
-
-    double radius = calc_radius(count);
-
-    coordinate point = {coordinates[1].x, coordinates[1].y};
-
-    printf("X: %d, Y: %d\n", coordinates[1].x, coordinates[1].y);
-
-    double dist = calc_dist(&point, center_x, center_y);
-    printf("Distance: %lf\n", dist);
+    eval_fill(count, coordinates);
+    eval_shape(count, coordinates);
 
     free(coordinates);
 
     return 0;
 }
 
-double calc_dist (coordinate* coordinate_point, double center_x, double center_y) {
+void read_district (int district[10][10]) {
+    int temp [10][10] = {{0,0,0,1,1,1,0,0,0,0},
+                        {0,1,1,1,1,1,1,0,0,0},
+                        {0,0,1,1,1,1,1,0,0,0},
+                        {0,1,1,1,1,1,1,0,0,0},
+                        {0,0,1,1,1,1,1,1,0,0},
+                        {0,0,1,1,1,1,1,1,1,0},
+                        {0,0,1,1,1,1,1,1,1,0},
+                        {0,0,0,1,1,1,1,0,0,0},
+                        {0,0,0,1,1,1,0,0,0,0},
+                        {0,0,0,0,1,0,0,0,0,0}};
 
-    double dist_x = (double) coordinate_point[0].x - center_x;
-    double dist_y = (double) coordinate_point[0].y - center_y;
-    printf("Dist x: %lf", dist_x);
-    printf("Dist y: %lf", dist_y);
-
-    return sqrt(dist_x*dist_x + dist_y*dist_y);
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            district[i][j] = temp[i][j];
+        }
+    }
 }
-
-double calc_radius (int count) {
-    return sqrt(count / (2 * M_PI));
-}
-
 coordinate* generate_coordinates(int rows, int cols, int district[rows][cols], int* count) {
     *count = 0; // Initialize count
 
@@ -83,6 +80,31 @@ coordinate* generate_coordinates(int rows, int cols, int district[rows][cols], i
 
     return coordinates;
 }
+void print_district (int district[10][10]) {
+    for (int i = 0; i<10; i++) {
+        for(int j = 0; j<10; j++) {
+            if (district[i][j] == 1 ) {
+                printf("%d", district[i][j]);
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+
+double eval_shape(int count, coordinate coordinates[count]) {
+
+    double  center_x = 0,
+            center_y = 0;
+
+    calc_center(&center_x, &center_y, coordinates, count);
+    double average_dist = calc_avg_dist(count, coordinates, center_x, center_y);
+    printf("average dist: %lf\n", average_dist);
+
+}
 
 void calc_center (double* center_x, double* center_y, coordinate* coordinates, int count) {
     int sum_of_x = 0;
@@ -97,34 +119,73 @@ void calc_center (double* center_x, double* center_y, coordinate* coordinates, i
     *center_y = (double) sum_of_y/count;
 }
 
-void read_district (int district[10][10]) {
-    int temp [10][10] = {{0,0,1,0,0,0,0,0,0,0},
-                        {0,1,1,1,0,0,0,0,0,0},
-                        {0,0,1,1,1,0,0,0,0,0},
-                        {0,1,1,1,1,1,1,0,0,0},
-                        {0,0,1,1,1,1,1,1,0,0},
-                        {0,0,1,1,1,1,1,1,1,0},
-                        {0,0,1,1,1,1,1,1,1,0},
-                        {1,1,1,1,1,1,1,1,1,1},
-                        {1,1,1,1,1,1,1,1,0,0},
-                        {1,1,1,1,1,1,1,1,0,0}};
+double calc_avg_dist(int count, coordinate coordinates[count], double center_x, double center_y) {
+    double dist = 0;
 
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            district[i][j] = temp[i][j];
-        }
+    for (int i = 0; i < count; i++) {
+        coordinate coordinate_point = {coordinates[i].x, coordinates[i].y};
+        dist += calc_dist (&coordinate_point, center_x, center_y);
+        printf("distance akk [%d]: %lf\n", i, dist);
     }
+
+    return dist/count;
 }
 
-void print_district (int district[10][10]) {
-    for (int i = 0; i<10; i++) {
-        for(int j = 0; j<10; j++) {
-            if (district[i][j] == 1 ) {
-                printf("%d", district[i][j]);
-            } else {
-                printf(" ");
-            }
-        }
-        printf("\n");
-    }
+double calc_dist (coordinate* coordinate_point, double center_x, double center_y) {
+
+    double dist_x = (double) coordinate_point[0].x - center_x;
+    double dist_y = (double) coordinate_point[0].y - center_y;
+
+    double dist = sqrt(dist_x*dist_x + dist_y*dist_y);
+    printf("distance: %lf\n", dist);
+
+    return dist;
 }
+
+
+
+double eval_fill(int count, coordinate coordinates[count]) {
+    double radius = calc_radius(count);
+    printf("Radius: %lf\n", radius);
+
+    check_coordinate(count, coordinates, radius);
+}
+
+double calc_radius (int count) {
+    return sqrt(count / (2 * M_PI));
+}
+
+void check_coordinate(int count, coordinate coordinates[count], double radius) {
+
+    int in_cirkle = 0;
+    int out_cirkle = 0;
+    double dist = 0;
+
+    double  center_x = 0,
+            center_y = 0;
+
+    calc_center(&center_x, &center_y, coordinates, count);
+
+    for (int i = 0; i < count; i++) {
+        coordinate coordinate_point = {coordinates[i].x, coordinates[i].y};
+        dist = calc_dist(&coordinate_point, center_x, center_y);
+        if (dist < radius) {
+            in_cirkle++;
+        } else {
+            out_cirkle++;
+        }
+    }
+    printf("in cirkle: %d\n", in_cirkle);
+    printf("out cirkle: %d\n", out_cirkle);
+
+}
+
+
+
+
+
+
+
+
+
+
