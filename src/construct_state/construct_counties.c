d546ea7922112ba2205fca5bb6edb555000b8dc3
Cilas <Cilas@DominoWWW.com>
2024-12-04 14:58:12 +0100

    Get parties

--- src/construct_state/construct_counties.c
+++ src/construct_state/construct_counties.c
@@ -11,28 +11,57 @@ void construct_counties(void) {
 
     FILE* file = open_file(FILE_COUNTIES, "r");
 
-    int number_of_parties = calc_number_of_parties(file);
-    printf("Number of parties: %d\n", number_of_parties);
+    char parties[MAX_NUMBER_OF_PARTIES][4] = {""};
+    get_unique_parties(file, parties);
+
+    for (int i = 0; i < MAX_NUMBER_OF_PARTIES; i++) {
+        printf("%s\n", parties[i]);
+    }
+
 
 
 
 }
 
-int calc_number_of_parties(FILE* file) {
+void get_unique_parties(FILE* file, char parties[MAX_NUMBER_OF_PARTIES][4]) {
     //reset file pointer
     rewind(file);
     //skip first line
     fscanf(file, "%*[^\n]\n");
 
 
-    char scaned_party[4];
-    char scaned_parties[MAX_NUMBER_OF_PARTIES][4];
+    char scanned_party[4];
+    char scanned_parties[MAX_NUMBER_OF_PARTIES][4] = {""};
 
     //loop through file until end
     while (feof(file) == 0) {
         //Scan line, for Party
-        fscanf(file, "%[^\t] \t %*i \t %[^\t] \t %*i\n", &scaned_party);
-    }
+        fscanf(file, "%*[^\t] \t %*i \t %s \t %*i\n", &scanned_party);
 
-    return 10000;
+        //Count unique parties found so far
+        int number_of_parties = 0;
+        for (int i = 0; i < sizeof(scanned_parties) / sizeof(scanned_parties[0]); i++) {
+            if (strlen(scanned_parties[i]) == 0) continue;
+            number_of_parties++;
+        }
+
+        //continue if scanned_party is not unique
+        if (is_string_in_array(scanned_parties, scanned_party, number_of_parties)) continue;
+
+        strcat(scanned_parties[number_of_parties], scanned_party);
+
+    }
+    for (int i = 0; i < MAX_NUMBER_OF_PARTIES; i++) strcat(parties[i], scanned_parties[i]);
+}
+
+int is_string_in_array(char string[MAX_NUMBER_OF_PARTIES][4], char to_find[], int string_length) {
+    int result = 0;
+
+    for (int i = 0; i < string_length; i++) {
+        if (!strcmp(string[i], to_find)) {
+            result = 1;
+            break;
+        }
+    }
+    return result;
 }
--- src/construct_state/construct_state_header.h
+++ src/construct_state/construct_state_header.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <stdio.h>
+#include "header.h"
 
 void construct_counties(void);
 FILE *open_file(const char *file_name, const char *mode);
-int calc_number_of_parties(FILE* file);
+void get_unique_parties(FILE* file, char parties[MAX_NUMBER_OF_PARTIES][4]);
+int is_string_in_array(char string[MAX_NUMBER_OF_PARTIES][4], char to_find[], int string_length);
