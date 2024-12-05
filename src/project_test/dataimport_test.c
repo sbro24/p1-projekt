0b49b8254ca3b8cf6c9b22ecb512e943cd3c6b6b
Simon Bro <sib24@student.aau.dk>
2024-12-05 14:46:48 +0100

    added unit test dataimport_test.c

---
+++ src/project_test/dataimport_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dataimporter.h"
+#include "construct_state_header.h"
+
+int main(void) {
+
+    FILE *file = open_file("FILE_NAME", 'r');
+    filesize f = ScanFile(file);
+    char **dataStruct = Allocate2dCHARarray(f.lineCount, f.longestLine);
+    ReadFileDataInto2dCHARarray(file, dataStruct, f.lineCount, f.longestLine, '\n');
+    fclose(file);
+    int **resultArray = ConvertToInt(dataStruct,f.lineCount,f.longestLine);
+    Print2dArrayCHAR(dataStruct,f.lineCount,f.longestLine);
+    Free2dArrayCHAR(dataStruct,f.lineCount);
+    Free2dArrayINT(resultArray,f.lineCount);
+
+    return 0;
+}
