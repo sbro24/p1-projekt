5bca858d3631dbe5fbd8564336fa0828f2c55bd9
Skrimma <hhjart24@student.aau.dk>
2024-12-06 14:43:55 +0100

    Vote shit

--- src/lib/calculate_proportionality/gallagher_index.c
+++ src/lib/calculate_proportionality/gallagher_index.c
@@ -3,6 +3,13 @@
 #include "calculate_proportionality.h"
 #include "header.h"
 
+
+/* A function that calculates the Gallagher-index of the given state.
+ * It has to first sum up the number of votes and seats per party,
+ * then it calculates the percentages of votes and seats, and then the difference squared,
+ * further, it halves the sum of the difference squared, and finally,
+ * it calculates the square root of the half of sum of difference squared,
+ * which is the desired Gallagher-index. */
 double gallagher_index(state_t state) {
     int total_votes = 0;
     int total_seats = 0;
@@ -13,7 +20,6 @@ double gallagher_index(state_t state) {
 
 
     for(int i = 0; i < MAX_NUMBER_OF_PARTIES; i++) {
-        // Calculate
         total_votes += state.total_votes[i];
         total_seats += state.number_of_seats[i];
         vote_percentages[i] = 100.00 * state.total_votes[i] / total_votes;
