2be45ec036241a4cb8a84b798058d264fbdf0097
Skrimma <hhjart24@student.aau.dk>
2024-12-12 10:04:29 +0100

    Output Shit

---
+++ src/lib/Output/output.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "output.h"
+#include "header.h"
+#include "calculate_proportionality.h"
+
+void output() {
+    output_eval_map();
+    output_gallagher_index();
+}
+
+
+void output_eval_map(double eval_map) {
+
+}
+
+
+void output_gallagher_index(double gallagher_index) {
+
+
+}
+
+
---
+++ src/lib/Output/output.h
@@ -0,0 +1,15 @@
+
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include "header.h"
+
+void output();
+
+void output_eval_map();
+
+void output_gallagher_index();
+
+
+
+#endif //OUTPUT_H
