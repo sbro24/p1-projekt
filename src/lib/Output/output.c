78df3cea045740f87d043afc25aa82316b9dbe2f
Skrimma <hhjart24@student.aau.dk>
2024-12-12 14:09:32 +0100

    Output Shit

--- src/lib/Output/output.c
+++ src/lib/Output/output.c
@@ -2,14 +2,22 @@
 #include "output.h"
 #include "header.h"
 
-
+// Function that will run in main, which prints out the desired output of the program
 void output(state_t state, char parties[MAX_NUMBER_OF_PARTIES][4], double eval_map, double gallagher_index) {
+    // Function that calculates and prints vote percentages and seats per party
     output_vote_percentages_and_seats(state, parties);
-    output_eval_map(eval_map);
-    output_gallagher_index(gallagher_index);
-    output_final_score(eval_map, reformat_gallagher_index(gallagher_index));
+
+    // Function that prints the eval_map index
+    print_eval_map_index(eval_map);
+
+    // Function that prints the gallagher-index
+    print_gallagher_index(gallagher_index);
+
+    // Function that prints the final score
+    print_final_score(eval_map, reformat_gallagher_index(gallagher_index));
 }
 
+// Function that calculates and prints vote percentages and seats per party
 void output_vote_percentages_and_seats(state_t state, char parties[MAX_NUMBER_OF_PARTIES][4]) {
     // Initialize variables
     int total_votes = 0;
@@ -32,31 +40,37 @@ void output_vote_percentages_and_seats(state_t state, char parties[MAX_NUMBER_OF
     printf("\n");
 }
 
-void output_eval_map(double eval_map) {
+// Function that prints the eval_map index
+void print_eval_map_index(double eval_map) {
 
     printf("eval_map: %lf\n", eval_map);
     printf("\n");
 }
 
+// Function that prints the gallagher-index
+void print_gallagher_index(double gallagher_index) {
+
+    printf("gallagher_index: %lf\n", gallagher_index);
+    printf("\n");
+}
+
+// Function that reformats the gallagher-index, so that it can be used to calculate a final score
 double reformat_gallagher_index(double gallagher_index) {
     gallagher_index = 100.00 - gallagher_index;
 
     return gallagher_index;
 }
 
-void output_gallagher_index(double gallagher_index) {
-
-    printf("gallagher_index: %lf\n", gallagher_index);
-    printf("\n");
-}
-
-double calculate_final_score(double eval_map, double gallagher_index) {
-    double final_score = (eval_map + gallagher_index) / 2;
+// Function that calculates the final score, by finding the mean number
+// of the eval_map- and reformated gallagher indexes
+double calculate_final_score(double eval_map, double reformated_gallagher_index) {
+    double final_score = (eval_map + reformated_gallagher_index) / 2;
 
     return final_score;
 }
 
-void output_final_score(double eval_map, double gallagher_index) {
-    double final_score = calculate_final_score(eval_map, gallagher_index);
+// Function that prints the final score
+void print_final_score(double eval_map, double reformated_gallagher_index) {
+    double final_score = calculate_final_score(eval_map, reformated_gallagher_index);
     printf("final_score: %lf\n", final_score);
 }
--- src/lib/Output/output.h
+++ src/lib/Output/output.h
@@ -8,15 +8,15 @@ void output(state_t state, char parties[MAX_NUMBER_OF_PARTIES][4], double eval_m
 
 void output_vote_percentages_and_seats(state_t state, char parties[MAX_NUMBER_OF_PARTIES][4]);
 
-void output_eval_map(double eval_map);
+void print_eval_map_index(double eval_map);
 
 double reformat_gallagher_index(double gallagher_index);
 
-void output_gallagher_index(double gallagher_index);
+void print_gallagher_index(double gallagher_index);
 
-double calculate_final_score(double eval_map, double gallagher_index);
+double calculate_final_score(double eval_map, double reformated_gallagher_index);
 
-void output_final_score(double eval_map, double gallagher_index);
+void print_final_score(double eval_map, double reformated_gallagher_index);
 
 
 #endif //OUTPUT_H
